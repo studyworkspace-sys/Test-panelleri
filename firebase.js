1face5d2a95fb98ce6bfab99fa3b10a01669a4c6
studyworkspace-sys <study.workspace@konyaisabet.k12.tr>
2026-05-13 16:23:44 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,1 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAi0yhvBY9lz_-Lc3GXVSzcq4596d0zEao",
-  authDomain: "odev-takip-sistemi-bf3fb.firebaseapp.com",
-  projectId: "odev-takip-sistemi-bf3fb",
-  storageBucket: "odev-takip-sistemi-bf3fb.firebasestorage.app",
-  messagingSenderId: "1029015236060",
-  appId: "1:1029015236060:web:7f54d0770aea5768bb7319"
-};
-
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
